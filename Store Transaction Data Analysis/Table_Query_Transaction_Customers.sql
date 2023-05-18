994f5c4b65edf8b35d63381222da1428e04313df
Your Name <seunkilankois@gmail.com>
2023-05-18 16:22:05 +0100

    Updated Portfolio

--- Store Transaction Data Analysis/Table_Query_Transaction_Customers.sql
+++ Store Transaction Data Analysis/Table_Query_Transaction_Customers.sql
@@ -28,41 +28,52 @@ FROM [transaction]
 GROUP BY store_location
 ORDER BY Total_Quantity_Purchased DESC
 
+-- Present the table showing only Purchase Date, Store Location, Quantity Purchased and Sales
 SELECT purchase_date, store_location, quantity_purchased, sales
 From [transaction]
 
+-- Present the table showing only Purchase Date, Store Location, Quantity Purchased and Sales 
+-- and the data is ordered showing the highest Quantity Purchased and Sales first
 SELECT purchase_date, store_location, quantity_purchased, sales
 From [transaction]
 Order by quantity_purchased DESC, sales DESC
 
+-- Show the transaction that took place on 2021-10-16 with the highest Quantity Purchased and Sales
+-- shown first
 SELECT purchase_date, store_location, quantity_purchased, sales
 From [transaction]
 WHERE purchase_date = '2021-10-16'
 Order by quantity_purchased DESC, sales DESC
 
+-- Show Total Purchase and Sales in each day with the highest shown first
 SELECT purchase_date, sum(quantity_purchased) as TotalPurchase, sum(sales) as TotalSales
 From [transaction]
 GROUP by purchase_date
 Order by TotalPurchase DESC
 
+-- Show the Customer table
 SELECT *
 From customers
 
+-- Join the Transaction and Customer table together 
 SELECT *
 From [transaction]
 Join customers
 ON [transaction].customer_id = customers.customer_id
 
+-- Filtered some important columns out of the joined Transaction and Customer Table
 SELECT [transaction].customer_id, purchase_date, store_location, quantity_purchased, sales, customer_lifestage, customer_premium
 From [transaction]
 Join customers
 ON [transaction].customer_id = customers.customer_id
 
+-- Filtered out the kind of Customer Premium in each Store Location
 SELECT [transaction].customer_id, store_location, customer_premium
 From [transaction]
 Join customers
 ON [transaction].customer_id = customers.customer_id
 
+-- Filtered out the Store Location against Customer Premium, Customer Lifestage and Sales
 SELECT [transaction].customer_id, store_location, customer_premium, customer_lifestage, sales
 From [transaction]
 Join customers
