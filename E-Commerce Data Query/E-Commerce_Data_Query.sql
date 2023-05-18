ebcaa8c72a0ffaf2e75ea2fb665adda62cc5f887
Your Name <seunkilankois@gmail.com>
2023-05-18 14:32:32 +0100

    Profile Updated

--- E-Commerce Data Query/E-Commerce_Data_Query.sql
+++
@@ -1,8 +0,0 @@
--- Check Number of Rows
-SELECT COUNT(*)
-FROM [Ecommerce_Data-1]
-
--- Table Rearranged
-SELECT "Date", CustomerID, InvoiceNo, StockCode, "Description", Quantity, UnitPrice, Country, "Hour"
-FROM [Ecommerce_Data-1]
-ORDER BY "Date" DESC
