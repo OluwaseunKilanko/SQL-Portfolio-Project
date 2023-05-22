b722c297f4ae5b4030296456decf13a9c16b27ae
Your Name <seunkilankois@gmail.com>
2023-05-23 00:33:39 +0100

    Updated Analysis E-Commerce

--- E-Commerce Data Analysis/E-Commerce_Data_Query.sql
+++ E-Commerce Data Analysis/E-Commerce_Data_Query.sql
@@ -6,3 +6,41 @@ FROM [Ecommerce_Data-1]
 SELECT "Date", CustomerID, InvoiceNo, StockCode, "Description", Quantity, UnitPrice, Country, "Hour"
 FROM [Ecommerce_Data-1]
 ORDER BY "Date" DESC
+
+-- Show the countries that has their data in the table
+Select Distinct Country
+From [Ecommerce_Data-1]
+Order BY Country
+
+-- Show the number of transaction that took place in each country
+Select Country, COUNT(Country) TotalTransaction
+From [Ecommerce_Data-1]
+GROUP BY Country
+ORDER BY TotalTransaction   DESC
+
+-- Show the number of transaction that took place in each day
+Select [Date], COUNT([Date]) TransactionToday
+From [Ecommerce_Data-1]
+GROUP BY [Date]
+ORDER BY [Date]   DESC
+
+-- Show the date with higher trasaction
+Select [Date], COUNT([Date]) TransactionToday
+From [Ecommerce_Data-1]
+GROUP BY [Date]
+ORDER BY [TransactionToday]   DESC
+
+-- Calculate the  amount of sales per transaction
+SELECT Date, Description, Quantity, UnitPrice, (Quantity * UnitPrice) Sales, Country
+FROM [Ecommerce_Data-1]
+ORDER BY Date DESC
+
+-- Show the total amount of sales and transaction per country
+SELECT Country, SUM((Quantity * UnitPrice)) TotalSales, COUNT(Country) TotalTransaction
+FROM [Ecommerce_Data-1]
+GROUP BY Country
+ORDER BY TotalSales DESC
+
+-- Show the total amount of sales in the data
+Select SUM((Quantity * UnitPrice)) TotalOverallSales
+FROM [Ecommerce_Data-1]
